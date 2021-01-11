a717f2f8f5400f99f8408782866e9397f149910c
sherylynn <352281674@qq.com>
2021-01-11 16:26:12 +0800

    move TARGET_

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,7 +4,8 @@ $(warning !!!TARGET_OUT_ETC!!! is-- $(TARGET_OUT_ETC))
 $(warning !!!TARGET_ROOT_OUT!!! is-- $(TARGET_ROOT_OUT))
 $(warning !!!TARGET_OUT_VENDOR_ETC!!! is-- $(TARGET_OUT_VENDOR_ETC))
 
-TARGET_VENDOR_OVERLAY := out/target/product/raphael/system/product/vendor_overlay/29
+#TARGET_VENDOR_OVERLAY := out/target/product/raphael/system/product/vendor_overlay/29
+# define this in Boardconfig.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mi_thermald.rc
@@ -24,8 +25,11 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 #LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+ifdef TARGET_VENDOR_OVERLAY
 LOCAL_MODULE_PATH  := $(TARGET_VENDOR_OVERLAY)/etc/init/hw
-#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+else
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+endif
 #LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -37,6 +41,9 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
 #LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+ifdef TARGET_VENDOR_OVERLAY
 LOCAL_MODULE_PATH  := $(TARGET_VENDOR_OVERLAY)/etc
+else
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+endif
 include $(BUILD_PREBUILT)
